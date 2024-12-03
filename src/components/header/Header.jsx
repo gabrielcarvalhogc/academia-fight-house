c97a33e80297511152094c73db1c82569e4721d1
Gabriel Carvalho <gabriel225gc@gmail.com>
2024-12-03 16:01:53 -0300

    Feat: Melhorada a navegação entre as seções

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -7,33 +7,74 @@ import './Header.css';
 
 function Header() {
     const [expanded, setExpanded] = useState(false);
+    const [headerHeight, setHeaderHeight] = useState(0);
 
-    const handleLinkClick = () => setExpanded(false);
+    useEffect(() => {
+        // Calcula a altura do header para ajustar o scroll
+        const headerElement = document.getElementById('navID');
+        if (headerElement) {
+            setHeaderHeight(headerElement.offsetHeight);
+        }
+    }, []);
 
-    return (   
-        <header> 
-            <Navbar 
-                expand="lg" 
-                fixed="top" 
-                className='header' 
-                id="navID" 
-                expanded={expanded}
-            >
+    const handleLinkClick = (event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const offsetTop = targetElement.offsetTop - headerHeight - 10;
+            window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+        }
+        setExpanded(false);
+    };
+
+    return (
+        <header>
+            <Navbar expand="lg" fixed="top" className="header" id="navID" expanded={expanded}>
                 <Container>
                     <Navbar.Brand href="/">
-                        <img src={LogoFightHouse} alt="Logo da academia fight house" height={85} width={80} />
+                        <img src={LogoFightHouse} alt="Logo da academia Fight House" height={85} width={80} />
                     </Navbar.Brand>
-                    <Navbar.Toggle 
-                        aria-controls="basic-navbar-nav" 
+                    <Navbar.Toggle
+                        aria-controls="basic-navbar-nav"
                         onClick={() => setExpanded(!expanded)}
                     />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto fw-bold ">
-                            <Nav.Link href="#sobre" onClick={handleLinkClick} className='text-black'>Sobre</Nav.Link>
-                            <Nav.Link href="#modalidades" onClick={handleLinkClick} className='text-black'>Modalidades</Nav.Link>
-                            <Nav.Link href="#nossosPlanos" onClick={handleLinkClick} className='text-black'>Nossos planos</Nav.Link>
-                            <Nav.Link href="#localizacao" onClick={handleLinkClick} className='text-black'>Localização</Nav.Link>
-                            <Nav.Link href="#contato" onClick={handleLinkClick} className='text-black'>Contato</Nav.Link>                           
+                        <Nav className="me-auto fw-bold">
+                            <Nav.Link
+                                href="#sobre"
+                                onClick={(e) => handleLinkClick(e, 'sobre')}
+                                className="text-black"
+                            >
+                                Sobre
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#modalidades"
+                                onClick={(e) => handleLinkClick(e, 'modalidades')}
+                                className="text-black"
+                            >
+                                Modalidades
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#nossosPlanos"
+                                onClick={(e) => handleLinkClick(e, 'nossosPlanos')}
+                                className="text-black"
+                            >
+                                Nossos Planos
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#localizacao"
+                                onClick={(e) => handleLinkClick(e, 'localizacao')}
+                                className="text-black"
+                            >
+                                Localização
+                            </Nav.Link>
+                            <Nav.Link
+                                href="#contato"
+                                onClick={(e) => handleLinkClick(e, 'contato')}
+                                className="text-black"
+                            >
+                                Contato
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
