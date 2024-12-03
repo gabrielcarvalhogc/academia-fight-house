7b22cf27e212444bfe7d54d4ca803d5fd2cdba14
Gabriel Carvalho <gabriel225gc@gmail.com>
2024-12-03 15:09:17 -0300

    Feat: fivicon e estilização na raiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import './App.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Header from './components/header/Header';
@@ -13,7 +14,7 @@ function App() {
   return (
     <>
       <Header/>
-      <main style={{backgroundColor: 'var(--black)'}}>
+      <main>
         <SobreNos/>
         <Modalidades/>
         <Planos/>
